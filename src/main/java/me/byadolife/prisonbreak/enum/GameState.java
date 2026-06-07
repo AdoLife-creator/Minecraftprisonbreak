ab91e49d7669716e829b474aba7e32ea3f866eac
AdoLife-creator <order560878@gmail.com>
2026-06-07 15:43:43 +0300

    Delete src/main/java/me/byadolife/prisonbreak/enum directory

--- src/main/java/me/byadolife/prisonbreak/enum/GameState.java
+++
@@ -1,8 +0,0 @@
-package me.byadolife.prisonbreak.models;
-
-public enum GameState {
-    WAITING,      // Oyuncu bekleniyor
-    COUNTDOWN,    // Geri sayım
-    ACTIVE,       // Oyun aktif
-    ENDING        // Oyun bitiyor
-}
