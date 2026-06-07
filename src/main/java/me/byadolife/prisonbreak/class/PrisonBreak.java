f5437facb698b97c1a99a254634717f37747d7fa
AdoLife-creator <order560878@gmail.com>
2026-06-07 15:55:41 +0300

    Create PrisonBreak.java

---
+++ src/main/java/me/byadolife/prisonbreak/class/PrisonBreak.java
@@ -0,0 +1,109 @@
+',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,139] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,151] '{' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[18,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,132] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,139] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,151] '{' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[29,43] reached end of file while parsing
+[INFO] 36 errors 
+[INFO] -------------------------------------------------------------
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  20.180 s
+[INFO] Finished at: 2026-06-07T12:53:08Z
+[INFO] ------------------------------------------------------------------------
+Error:  Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project PrisonBreak: Compilation failure: Compilation failure: 
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[1,1] class, interface, enum, or record expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[3,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[4,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,132] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[6,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,139] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[7,151] '{' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[17,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[18,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,132] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,137] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,148] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,154] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,155] '}' or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,158] ',', '}', or ';' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[20,174] '(' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,6] <identifier> expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,139] enum constant expected here
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[21,151] '{' expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[29,43] reached end of file while parsing
+Error:  -> [Help 1]
+Error:  
+Error:  To see the full stack trace of the errors, re-run Maven with the -e switch.
+Error:  Re-run Maven using the -X switch to enable full debug logging.
+Error:  
+Error:  For more information about the errors and possible solutions, please read the following articles:
+Error:  [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+Error: Process completed with exit code 1.package me.byadolife.prisonbreak;
+
+import me.byadolife.prisonbreak.commands.PBCommand;
+import me.byadolife.prisonbreak.listeners.JoinListener;
+import me.byadolife.prisonbreak.listeners.TeamMenuListener;
+import me.byadolife.prisonbreak.managers.TeamManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrisonBreak extends JavaPlugin {
+
+    private static PrisonBreak instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        TeamManager.setup();
+
+        getCommand("pb").setExecutor(new PBCommand());
+
+        getServer().getPluginManager().registerEvents(new TeamMenuListener(), this);
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public static PrisonBreak getInstance() {
+        return instance;
+    }
+}
