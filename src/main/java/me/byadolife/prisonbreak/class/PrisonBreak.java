b65d72035b800730e776c193fc1757e86139a347
AdoLife-creator <order560878@gmail.com>
2026-06-07 22:38:50 +0300

    Update PrisonBreak.java

--- src/main/java/me/byadolife/prisonbreak/class/PrisonBreak.java
+++ src/main/java/me/byadolife/prisonbreak/class/PrisonBreak.java
@@ -4,6 +4,7 @@ import me.byadolife.prisonbreak.commands.PBCommand;
 import me.byadolife.prisonbreak.listeners.JoinListener;
 import me.byadolife.prisonbreak.listeners.TeamMenuListener;
 import me.byadolife.prisonbreak.managers.TeamManager;
+import me.byadolife.prisonbreak.managers.TabManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PrisonBreak extends JavaPlugin {
@@ -22,10 +23,13 @@ public final class PrisonBreak extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new TeamMenuListener(), this);
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
+
+        getLogger().info("PrisonBreak aktif edildi!");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("PrisonBreak kapatildi!");
     }
 
     public static PrisonBreak getInstance() {
