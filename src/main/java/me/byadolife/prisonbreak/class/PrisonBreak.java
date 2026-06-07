d16c892f858db6f845f707fdb6e604a4de2897c8
AdoLife-creator <order560878@gmail.com>
2026-06-07 23:11:14 +0300

    Update PrisonBreak.java

--- src/main/java/me/byadolife/prisonbreak/class/PrisonBreak.java
+++ src/main/java/me/byadolife/prisonbreak/class/PrisonBreak.java
@@ -2,7 +2,9 @@ package me.byadolife.prisonbreak;
 
 import me.byadolife.prisonbreak.commands.PBCommand;
 import me.byadolife.prisonbreak.listeners.JoinListener;
+import me.byadolife.prisonbreak.listeners.QuitListener;
 import me.byadolife.prisonbreak.listeners.TeamMenuListener;
+import me.byadolife.prisonbreak.managers.TabManager;
 import me.byadolife.prisonbreak.managers.TeamManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,29 +19,17 @@ public final class PrisonBreak extends JavaPlugin {
         saveDefaultConfig();
 
         TeamManager.setup();
+        TabManager.reset(null); // güvenli init
 
-        PBCommand pbCommand = new PBCommand();
+        getCommand("pb").setExecutor(new PBCommand());
 
-        getCommand("pb").setExecutor(pbCommand);
-        getCommand("pb").setTabCompleter(pbCommand);
-
-        getServer().getPluginManager().registerEvents(
-                new TeamMenuListener(),
-                this
-        );
-
-        getServer().getPluginManager().registerEvents(
-                new JoinListener(),
-                this
-        );
-
-        getLogger().info("PrisonBreak enabled.");
+        getServer().getPluginManager().registerEvents(new TeamMenuListener(), this);
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new QuitListener(), this);
     }
 
     @Override
-    public void onDisable() {
-        getLogger().info("PrisonBreak disabled.");
-    }
+    public void onDisable() {}
 
     public static PrisonBreak getInstance() {
         return instance;
