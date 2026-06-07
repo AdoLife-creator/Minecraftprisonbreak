9ac67df23b3709a773c87954f856f4f3ab1d3e39
AdoLife-creator <order560878@gmail.com>
2026-06-07 15:52:37 +0300

    Update PrisonBreak.java

--- src/main/java/me/byadolife/prisonbreak/PrisonBreak.java
+++ src/main/java/me/byadolife/prisonbreak/PrisonBreak.java
@@ -1,35 +1,29 @@
-package me.byadolife.prisonbreak;
-
-import me.byadolife.prisonbreak.commands.PBCommand;
-import me.byadolife.prisonbreak.listeners.JoinListener;
-import me.byadolife.prisonbreak.listeners.TeamMenuListener;
-import me.byadolife.prisonbreak.managers.TeamManager;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class PrisonBreak extends JavaPlugin {
-
-    private static PrisonBreak instance;
-
-    @Override
-    public void onEnable() {
-        instance = this;
-
-        saveDefaultConfig();
-
-        TeamManager.setup();
-
-        getCommand("pb").setExecutor(new PBCommand());
-
-        getServer().getPluginManager().registerEvents(new TeamMenuListener(), this);
-        getServer().getPluginManager().registerEvents(new JoinListener(), this);
-    }
-
-    public static PrisonBreak getInstance() {
-        return instance;
-    }
-}    }
-
-    public LocationManager getLocationManager() {
-        return locationManager;
-    }
-}
+Error:  COMPILATION ERROR : 
+[INFO] -------------------------------------------------------------
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[30,6] class, interface, enum, or record expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[32,12] unnamed classes are a preview feature and are disabled by default.
+  (use --enable-preview to enable unnamed classes)
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[35,1] class, interface, enum, or record expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[1,1] unnamed class should not have package declaration
+[INFO] 4 errors 
+[INFO] -------------------------------------------------------------
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  17.296 s
+[INFO] Finished at: 2026-06-07T12:50:06Z
+[INFO] ------------------------------------------------------------------------
+Error:  Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project PrisonBreak: Compilation failure: Compilation failure: 
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[30,6] class, interface, enum, or record expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[32,12] unnamed classes are a preview feature and are disabled by default.
+Error:    (use --enable-preview to enable unnamed classes)
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[35,1] class, interface, enum, or record expected
+Error:  /home/runner/work/Minecraftprisonbreak/Minecraftprisonbreak/src/main/java/me/byadolife/prisonbreak/PrisonBreak.java:[1,1] unnamed class should not have package declaration
+Error:  -> [Help 1]
+Error:  
+Error:  To see the full stack trace of the errors, re-run Maven with the -e switch.
+Error:  Re-run Maven using the -X switch to enable full debug logging.
+Error:  
+Error:  For more information about the errors and possible solutions, please read the following articles:
+Error:  [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+Error: Process completed with exit code 1.
