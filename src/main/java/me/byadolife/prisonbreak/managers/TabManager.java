d461d713cce130fd8352b53013c2756f766e95bd
AdoLife-creator <order560878@gmail.com>
2026-06-07 23:09:58 +0300

    Update TabManager.java

--- src/main/java/me/byadolife/prisonbreak/managers/TabManager.java
+++ src/main/java/me/byadolife/prisonbreak/managers/TabManager.java
@@ -1,14 +1,43 @@
 package me.byadolife.prisonbreak.managers;
 
+import me.byadolife.prisonbreak.team.TeamType;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class TabManager {
 
-    public static void update(Player player) {
+    private static final HashMap<UUID, TeamType> teams = new HashMap<>();
 
-        player.setPlayerListHeaderFooter(
-                "§6§lPrisonBreak\n§7Minecraft Edition",
-                "\n§7play.server.com"
-        );
+    public static void setPrisoner(Player player) {
+        teams.put(player.getUniqueId(), TeamType.PRISONER);
+        update(player);
+    }
+
+    public static void setGuard(Player player) {
+        teams.put(player.getUniqueId(), TeamType.GUARD);
+        update(player);
+    }
+
+    public static void update(Player player) {
+        TeamType type = teams.get(player.getUniqueId());
+
+        if (type == null) {
+            player.setPlayerListName("§7" + player.getName());
+            return;
+        }
+
+        if (type == TeamType.PRISONER) {
+            player.setPlayerListName("§6[M] §f" + player.getName());
+        }
+
+        if (type == TeamType.GUARD) {
+            player.setPlayerListName("§9[G] §f" + player.getName());
+        }
+    }
+
+    public static void reset(Player player) {
+        teams.remove(player.getUniqueId());
     }
 }
