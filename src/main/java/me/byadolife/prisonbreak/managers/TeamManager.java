3b988a319303e88e8097f39c349dff0558cdc3dc
AdoLife-creator <order560878@gmail.com>
2026-06-07 23:56:25 +0300

    Update TeamManager.java

--- src/main/java/me/byadolife/prisonbreak/managers/TeamManager.java
+++ src/main/java/me/byadolife/prisonbreak/managers/TeamManager.java
@@ -1,24 +1,46 @@
 package me.byadolife.prisonbreak.managers;
 
+import me.byadolife.prisonbreak.team.TeamType;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class TeamManager {
 
+    private static final HashMap<UUID, TeamType> cache = new HashMap<>();
+
     public static void setup() {
-        // scoreboard kullanmıyoruz burada
+        // şimdilik boş (TabManager handle ediyor)
     }
 
-    public static void setPrisoner(Player p) {
-        TabManager.setPrisoner(p);
+    public static void setPrisoner(Player player) {
 
-        p.sendMessage("§6Mahkum takımına katıldın!");
-        p.performCommand("skin set yoshio0302");
+        TabManager.setPrisoner(player);
+        cache.put(player.getUniqueId(), TeamType.PRISONER);
+
+        player.sendMessage("§6Mahkum takımına katıldın!");
+
+        // SkinRestorer komutu
+        player.performCommand("skin set yoshio0302");
     }
 
-    public static void setGuard(Player p) {
-        TabManager.setGuard(p);
+    public static void setGuard(Player player) {
 
-        p.sendMessage("§9Gardiyan takımına katıldın!");
-        p.performCommand("skin set Clinkoo");
+        TabManager.setGuard(player);
+        cache.put(player.getUniqueId(), TeamType.GUARD);
+
+        player.sendMessage("§9Gardiyan takımına katıldın!");
+
+        // SkinRestorer komutu
+        player.performCommand("skin set Clinkoo");
+    }
+
+    public static TeamType getTeam(Player player) {
+        return cache.get(player.getUniqueId());
+    }
+
+    public static void clear(Player player) {
+        cache.remove(player.getUniqueId());
     }
 }
