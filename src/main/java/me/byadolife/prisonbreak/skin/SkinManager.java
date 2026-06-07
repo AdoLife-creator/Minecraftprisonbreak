ae2a3976b3bca514d78dfc1f53e07b882bdbc7b3
AdoLife-creator <order560878@gmail.com>
2026-06-07 23:51:31 +0300

    Update SkinManager.java

--- src/main/java/me/byadolife/prisonbreak/skin/SkinManager.java
+++ src/main/java/me/byadolife/prisonbreak/skin/SkinManager.java
@@ -6,12 +6,16 @@ import org.bukkit.entity.Player;
 public class SkinManager {
 
     public static void setPrisonerSkin(Player p) {
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                "skin set yoshio0302 " + p.getName());
+        Bukkit.dispatchCommand(
+                Bukkit.getConsoleSender(),
+                "skin set " + p.getName() + " yoshio0302"
+        );
     }
 
     public static void setGuardSkin(Player p) {
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                "skin set Clinkoo " + p.getName());
+        Bukkit.dispatchCommand(
+                Bukkit.getConsoleSender(),
+                "skin set " + p.getName() + " Clinkoo"
+        );
     }
 }
