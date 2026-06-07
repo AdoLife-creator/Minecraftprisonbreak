f16ef5fa492ffde4bf518034a0db3998575041ba
AdoLife-creator <order560878@gmail.com>
2026-06-07 22:37:17 +0300

    Update TeamMenuListener.java

--- src/main/java/me/byadolife/prisonbreak/TeamMenuListener.java
+++ src/main/java/me/byadolife/prisonbreak/TeamMenuListener.java
@@ -1,10 +1,14 @@
 package me.byadolife.prisonbreak.listeners;
 
 import me.byadolife.prisonbreak.gui.TeamMenu;
+import me.byadolife.prisonbreak.managers.SpawnManager;
 import me.byadolife.prisonbreak.managers.TeamManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.*;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class TeamMenuListener implements Listener {
@@ -12,23 +16,49 @@ public class TeamMenuListener implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
 
-        if(!event.getView().getTitle().equals(TeamMenu.TITLE))
+        if (!event.getView().getTitle().equals(TeamMenu.TITLE))
             return;
 
         event.setCancelled(true);
 
-        if(event.getCurrentItem() == null)
+        if (event.getCurrentItem() == null)
             return;
 
         Player player = (Player) event.getWhoClicked();
 
-        if(event.getCurrentItem().getType() == Material.ORANGE_CONCRETE) {
+        if (event.getCurrentItem().getType() == Material.ORANGE_CONCRETE) {
+
             TeamManager.setPrisoner(player);
+
+            Bukkit.dispatchCommand(
+                    Bukkit.getConsoleSender(),
+                    "skin set " + player.getName() + " yoshio0302"
+            );
+
+            Location loc =
+                    SpawnManager.get("spawns.mahkum");
+
+            if (loc != null)
+                player.teleport(loc);
+
             player.closeInventory();
         }
 
-        if(event.getCurrentItem().getType() == Material.BLUE_CONCRETE) {
+        if (event.getCurrentItem().getType() == Material.BLUE_CONCRETE) {
+
             TeamManager.setGuard(player);
+
+            Bukkit.dispatchCommand(
+                    Bukkit.getConsoleSender(),
+                    "skin set " + player.getName() + " Clinkoo"
+            );
+
+            Location loc =
+                    SpawnManager.get("spawns.gardiyan");
+
+            if (loc != null)
+                player.teleport(loc);
+
             player.closeInventory();
         }
     }
