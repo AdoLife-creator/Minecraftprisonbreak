e72b13d6ca0548e26c9b5f650a2be9dd48432118
AdoLife-creator <order560878@gmail.com>
2026-06-07 14:20:07 +0300

    Update import statements in PlayerListener.java

--- src/main/java/me/byadolife/prisonbreak/listener/PlayerListener.java
+++ src/main/java/me/byadolife/prisonbreak/listener/PlayerListener.java
@@ -1,8 +1,8 @@
 package me.byadolife.prisonbreak.listener;
 
-import dev.prisonbreak.managers.GameManager;
-import dev.prisonbreak.models.GamePlayer;
-import dev.prisonbreak.models.GameState;
+import me.byadolife.prisonbreak.manager.GameManager;
+import me.byadolife.prisonbreak.models.GamePlayer;
+import me.byadolife.prisonbreak.models.GameState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
