18943a83b4ebb4ee056b57ab209d81ad30ae974e
AdoLife-creator <order560878@gmail.com>
2026-06-08 15:53:35 +0300

    Update SpawnManager.java

--- src/main/java/me/byadolife/prisonbreak/managers/SpawnManager.java
+++ src/main/java/me/byadolife/prisonbreak/managers/SpawnManager.java
@@ -1,62 +1,25 @@
 package me.byadolife.prisonbreak.managers;
 
-import me.byadolife.prisonbreak.PrisonBreak;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 
 public class SpawnManager {
 
-    public static void saveSpawn(String name, Location loc) {
+    public static Location get(String key) {
 
-        FileConfiguration cfg =
-                PrisonBreak.getInstance().getConfig();
+        var cfg = PrisonBreak.getInstance().getConfig();
 
-        cfg.set("spawns." + name + ".world",
-                loc.getWorld().getName());
+        String path = "teams." + key + ".spawn";
 
-        cfg.set("spawns." + name + ".x",
-                loc.getX());
-
-        cfg.set("spawns." + name + ".y",
-                loc.getY());
-
-        cfg.set("spawns." + name + ".z",
-                loc.getZ());
-
-        cfg.set("spawns." + name + ".yaw",
-                loc.getYaw());
-
-        cfg.set("spawns." + name + ".pitch",
-                loc.getPitch());
-
-        PrisonBreak.getInstance().saveConfig();
-    }
-
-    public static Location getSpawn(String name) {
-
-        FileConfiguration cfg =
-                PrisonBreak.getInstance().getConfig();
-
-        if (!cfg.contains("spawns." + name + ".world"))
-            return null;
-
-        World world =
-                Bukkit.getWorld(
-                        cfg.getString("spawns." + name + ".world")
-                );
-
-        if (world == null)
-            return null;
+        if (!cfg.contains(path + ".world")) return null;
 
         return new Location(
-                world,
-                cfg.getDouble("spawns." + name + ".x"),
-                cfg.getDouble("spawns." + name + ".y"),
-                cfg.getDouble("spawns." + name + ".z"),
-                (float) cfg.getDouble("spawns." + name + ".yaw"),
-                (float) cfg.getDouble("spawns." + name + ".pitch")
+                Bukkit.getWorld(cfg.getString(path + ".world")),
+                cfg.getDouble(path + ".x"),
+                cfg.getDouble(path + ".y"),
+                cfg.getDouble(path + ".z"),
+                (float) cfg.getDouble(path + ".yaw"),
+                (float) cfg.getDouble(path + ".pitch")
         );
     }
 }
